ecab26086b293aee70b64d0fcf42ee66c4b64306
YoofiBP <joseph.brown-pobee@ashesi.edu.gh>
2021-01-01 14:01:32 +0000

    Completed project

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,53 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  playSound(int fileNumber) {
+    AudioCache player = AudioCache();
+    player.play('note$fileNumber.wav');
+  }
+
+  Expanded buildKey(int soundFile, Color color) => Expanded(
+        child: FlatButton(
+          color: color,
+          onPressed: () {
+            playSound(soundFile);
+          },
+        ),
+      );
+
+  List<Expanded> buildChildren() {
+    var properties = [
+      {"soundFile": 1, "color": Colors.red},
+      {"soundFile": 2, "color": Colors.orange},
+      {"soundFile": 3, "color": Colors.yellow},
+      {"soundFile": 4, "color": Colors.green},
+      {"soundFile": 5, "color": Colors.blue},
+      {"soundFile": 6, "color": Colors.indigo},
+      {"soundFile": 7, "color": Colors.purple},
+    ];
+
+    List<Expanded> keys = [];
+
+    properties.forEach((element) {
+      keys.add(buildKey(element["soundFile"], element["color"]));
+    });
+
+    return keys;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: buildChildren(),
+          ),
         ),
       ),
     );
